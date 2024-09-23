77e9531fc74e4f0f8fb0d5cff42aaa7d27b6ddc3
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-23 10:57:18 +0500

    Update QQ plot.py
    Optimised script:
    
    • Reduced excessive lines
    • Made script more pythonic

--- Scripts for Plots/QQ plot.py
+++ Scripts for Plots/QQ plot.py
@@ -1,23 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import probplot
+import pandas as pd
 
-def qq_plot(data, log=False):
+def qq_plot(x, log=False): # QQ Plot for stock returns
+  
     if log:
-        data = np.diff(np.log(data), axis=0)[1:]
+        data = np.log(x / x.shift(1)).dropna() # Returns
 
-    num_assets = data.shape[1]
-    
-    for n in range(num_assets):
-        asset_data = data[:, n]
-        
-        plt.figure()
-        plt.title("Q-Q Plot")
-        probplot(asset_data, plot=plt)
+    for column in data.columns:
+        plt.figure()  # Create a new figure for each plot
+        probplot(x[column], plot=plt)
         plt.grid(True, linestyle='--', alpha=0.7)
-        
+        plt.title(f"{column} Q-Q Plot")
         plt.show()
-
-# Assuming 'returns_df' is a DataFrame containing your return data
-# Example usage
+        
 qq_plot(result, log=True)  # Set log=True if log returns

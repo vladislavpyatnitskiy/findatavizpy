2fae4f1b17fa20432f5deca64cad86ca654d0c44
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-09-16 11:00:51 +0500

    Update Barplot.py
    Revived script and added grid to plot

--- Scripts for Plots/Barplot.py
+++ Scripts for Plots/Barplot.py
@@ -11,7 +11,7 @@ def bar_plt(y, s=None, e=None, col="blue", main=None): # Bar plot
     for ticker in y:
         if s is None and e is None:
             # When neither start date nor end date is defined
-            data = yf.download(ticker)
+            data = yf.download(ticker, start="2007-01-01")
         elif e is None:
             data = yf.download(ticker, start=s) # Only start date is defined
         elif s is None:
@@ -22,8 +22,8 @@ def bar_plt(y, s=None, e=None, col="blue", main=None): # Bar plot
 
         # Extract the Adjusted Close prices and add to the DataFrame
         if not data.empty:
-            p[ticker] = data['Adj Close']
-
+            p[ticker] = data[('Close', f'{ticker}')]
+            
     p = p.dropna() # Drop rows with NA values
 
     x = ((np.exp(np.sum(np.log(p / p.shift(1)).dropna())) - 1) * 100)
@@ -36,6 +36,7 @@ def bar_plt(y, s=None, e=None, col="blue", main=None): # Bar plot
     
     x.plot(kind='bar')
     plt.title(main)
+    plt.grid(True, linestyle=":", color="grey")
     plt.show()
     
 bar_plt(["AIG", "MET", "HIG", "UNM", "OMF"], s="2023-10-01",

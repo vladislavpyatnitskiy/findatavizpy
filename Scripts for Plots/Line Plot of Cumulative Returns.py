8b953379f14e2b22f5acba23d7c272fc5a43b35e
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-09-18 10:15:15 +0500

    Update Line Plot of Cumulative Returns.py
    Rewrote script:
    
    • Added yfinance library to get data

--- Scripts for Plots/Line Plot of Cumulative Returns.py
+++ Scripts for Plots/Line Plot of Cumulative Returns.py
@@ -1,10 +1,29 @@
+import yfinance as yf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def cmlplot(x):
+def cmlplot(y, s=None, e=None):
   
-  x = (np.exp(np.cumsum(np.log(x / x.shift(1)).dropna())) - 1) * 100
+  p = pd.DataFrame()  # Create an empty DataFrame
+
+  # Loop for data extraction & Set up statements for start and end dates
+  for ticker in y:
+    if s is None and e is None:
+      data = yf.download(ticker, start="2007-01-01") # None is defined
+    elif e is None:
+      data = yf.download(ticker, start=s) # Only start date is defined
+    elif s is None:
+      data = yf.download(ticker, end=e)  # When only end date is defined
+    else:
+      data = yf.download(ticker, start=s, end=e) # Both dates are defined
+      
+    if not data.empty:
+      p[ticker] = data[('Close', f'{ticker}')] # Assign tickers
+            
+    p = p.dropna() # Drop rows with NA values
+
+  x = (np.exp(np.cumsum(np.log(p / p.shift(1)).dropna())) - 1) * 100
   
   for column in x.columns:
         plt.figure()  # Create a new figure for each plot
@@ -12,6 +31,8 @@ def cmlplot(x):
         plt.title(column)
         plt.xlabel('Trading Days')
         plt.ylabel('Return (%)')
+        plt.grid(True, linestyle=":", color="grey")
+        plt.axhline(y=0, color="black")
         plt.show()
         
-cmlplot(result)
+cmlplot(["AAPL", "MSFT", "META", "GOOGL", "AMZN"], s = "2023-01-01")

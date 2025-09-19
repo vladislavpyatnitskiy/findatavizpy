58d466a4afdb553f891040846d48994bec762f63
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-09-19 11:02:14 +0500

    Update Drawdown.py
    Added yfinance library to get data directly from yahoo! finance

--- Scripts for Plots/Drawdown.py
+++ Scripts for Plots/Drawdown.py
@@ -1,10 +1,29 @@
+import yfinance as yf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def drawdown_plt(x):
+def drawdown_plt(y, s=None, e=None):
   
-  x = np.log(x / x.shift(1)).dropna() * 100
+  p = pd.DataFrame()  # Create an empty DataFrame
+
+  # Loop for data extraction & Set up statements for start and end dates
+  for ticker in y:
+    if s is None and e is None:
+      data = yf.download(ticker, start="2007-01-01") # None is defined
+    elif e is None:
+      data = yf.download(ticker, start=s) # Only start date is defined
+    elif s is None:
+      data = yf.download(ticker, end=e)  # When only end date is defined
+    else:
+      data = yf.download(ticker, start=s, end=e) # Both dates are defined
+      
+    if not data.empty:
+      p[ticker] = data[('Close', f'{ticker}')] # Assign tickers
+            
+    p = p.dropna() # Drop rows with NA values
+  
+  x = np.log(p / p.shift(1)).dropna() * 100
   
   x[x > 0] = 0
   
@@ -12,8 +31,10 @@ def drawdown_plt(x):
         plt.figure()  # Create a new figure for each plot
         plt.plot(x[column])
         plt.title(f"{column} Drawdown")
+        plt.grid(True, linestyle=":", color="grey")
+        plt.axhline(y=0, color="black")
         plt.xlabel('Trading Days')
         plt.ylabel('Negative Return (%)')
         plt.show()
         
-drawdown_plt(result)
+drawdown_plt(["AAPL", "MSFT", "META", "GOOGL", "AMZN"], s = "2023-01-01")

fcea21b86d3fbd5a26e420c36cb1cd494e8482f0
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-09-22 18:28:00 +0500

    Update Histogram.py
    Added yfinance library to get data directly from yahoo! finance

--- Scripts for Plots/Histogram.py
+++ Scripts for Plots/Histogram.py
@@ -1,11 +1,29 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import yfinance as yf
 
-def hist_plt(x, log_returns=True):
+def hist_plt(y, s=None, e=None):
   
-  if log_returns:
-    x = np.log(x / x.shift(1)).dropna()
+  p = pd.DataFrame()  # Create an empty DataFrame
+
+  # Loop for data extraction & Set up statements for start and end dates
+  for ticker in y:
+    if s is None and e is None:
+      data = yf.download(ticker, start="2007-01-01") # None is defined
+    elif e is None:
+      data = yf.download(ticker, start=s) # Only start date is defined
+    elif s is None:
+      data = yf.download(ticker, end=e)  # When only end date is defined
+    else:
+      data = yf.download(ticker, start=s, end=e) # Both dates are defined
+      
+    if not data.empty:
+      p[ticker] = data[('Close', f'{ticker}')] # Assign tickers
+            
+    p = p.dropna() # Drop rows with NA values
+  
+  x = np.log(p / p.shift(1)).dropna()
   
   for column in x.columns:
         plt.figure()  # Create a new figure for each plot
@@ -13,6 +31,7 @@ def hist_plt(x, log_returns=True):
         plt.title(column)
         plt.xlabel('Returns')
         plt.ylabel('Frequency')
+        plt.grid(True, linestyle=":", color="grey")
         plt.show()
 
-hist_plt(result) # Display
+hist_plt(["UNM", "MSFT", "META", "GOOGL", "AMZN"], s = "2023-01-01") # Display

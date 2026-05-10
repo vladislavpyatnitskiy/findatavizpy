495be524767d7b1911e7d96e7083adf7311c5400
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-05-10 22:44:13 +0500

    Refactor several_cml_rets for date handling and plotting
    Updated the several_cml_rets function to accept start and end dates for data extraction. Modified the plotting logic to handle cumulative returns correctly.

--- Scripts for Plots/Several Cumulative Returns.py
+++ Scripts for Plots/Several Cumulative Returns.py
@@ -1,20 +1,41 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import yfinance as yf
 
-def several_cml_rets(x, title=None): # Cumulative Returns of Stocks
+def several_cml_rets(x, s=None, e=None, title=None):
   
-  x = (np.exp(np.cumsum(np.log(x / x.shift(1)).dropna())) - 1) * 100
+  p = pd.DataFrame()  # Create an empty DataFrame
+
+  # Loop for data extraction & Set up statements for start and end dates
+  for ticker in x:
+    if s is None and e is None:
+      data = yf.download(ticker, start="2007-01-01")
+    elif e is None:
+      data = yf.download(ticker, start=s) 
+    elif s is None:
+      data = yf.download(ticker, end=e) 
+    else:
+      data = yf.download(ticker, start=s, end=e)
+
+    if not data.empty:
+      p[ticker] = data[('Close', f'{ticker}')]
+            
+  p = p.dropna() # Drop rows with NA values
   
+  p = (np.exp(np.cumsum(np.log(p / p.shift(1)).dropna())) - 1) * 100
+   
   plt.figure(figsize=(10, 6))
-    
-  for column in x.columns:
-    plt.plot(x.index, x[column], label=column)
-    plt.title(title)
-    plt.xlabel('Trading Days')
-    plt.ylabel('Return (%)')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+     
+  plt.plot(p, label=x)
+  plt.title(title)
+  plt.xlabel('Trading Days')
+  plt.ylabel('Return (%)')
+  plt.legend()
+  plt.grid(True)
+  plt.show()
 
-several_cml_rets(result, title="Performance of Insurance Stocks")
+several_cml_rets(
+  ["AIG", "MET", "HIG", "UNM", "OMF"], 
+  s="2023-10-01", title="Performance of Insurance Stocks"
+)

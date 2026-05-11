f0114a8eb3a6ca4c72a4689d93f5ea0f1506acb6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-05-11 11:19:23 +0500

    Enhance volatility_plt to include date range and tickers
    Modified the volatility_plt function to accept start and end dates for data extraction. Updated the plotting logic to use downloaded data for specified tickers.

--- Scripts for Plots/Volatility Plot.py
+++ Scripts for Plots/Volatility Plot.py
@@ -1,17 +1,37 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import yfinance as yf
 
-def volatility_plt(x):
+def volatility_plt(x, s=None, e=None):
   
-  x = np.log(x / x.shift(1)).dropna() * 100
+  p = pd.DataFrame()  # Create an empty DataFrame
+
+  # Loop for data extraction & Set up statements for start and end dates
+  for ticker in x:
+    if s is None and e is None:
+      data = yf.download(ticker, start="2007-01-01")
+    elif e is None:
+      data = yf.download(ticker, start=s) 
+    elif s is None:
+      data = yf.download(ticker, end=e) 
+    else:
+      data = yf.download(ticker, start=s, end=e)
+
+    if not data.empty:
+      p[ticker] = data[('Close', f'{ticker}')]
+            
+  p = p.dropna() # Drop rows with NA values
   
-  for column in x.columns:
+  p = np.log(p / p.shift(1)).dropna() * 100
+  
+  for column in p.columns:
         plt.figure()  # Create a new figure for each plot
-        plt.plot(x[column])
+        plt.plot(p[column])
         plt.title(f"{column} Volatility")
         plt.xlabel('Trading Days')
         plt.ylabel('Return (%)')
+        plt.grid(True, linestyle=":", color="grey")
         plt.show()
         
-volatility_plt(result)
+volatility_plt(["AIG", "MET", "HIG", "UNM", "OMF"], s="2023-10-01")

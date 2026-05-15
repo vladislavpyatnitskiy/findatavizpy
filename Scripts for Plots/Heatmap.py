56d80da7e267fd20641e7335b6bb54f4cbbbcf5e
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-05-15 11:06:57 +0500

    Add correlation method parameter to heat_map function

--- Scripts for Plots/Heatmap.py
+++ Scripts for Plots/Heatmap.py
@@ -4,35 +4,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def heat_map(y, s=None, e=None): # Heatmap
+def heat_map(y, s=None, e=None, method="pearson"):
   
-    p = pd.DataFrame()  # Create an empty DataFrame
+  p = pd.DataFrame()  # Create an empty DataFrame
 
     # Loop for data extraction & Set up statements for start and end dates
-    for ticker in y:
-        if s is None and e is None:
-            # When neither start date nor end date is defined
-            data = yf.download(ticker)
-        elif e is None:
-            data = yf.download(ticker, start=s) # Only start date is defined
-        elif s is None:
-            data = yf.download(ticker, end=e)  # When only end date is defined
-        else:
-            # When both start date and end date are defined
-            data = yf.download(ticker, start=s, end=e)
+  for ticker in y:
+    if s is None and e is None:
+        # When neither start date nor end date is defined
+      data = yf.download(ticker, start="2007-01-01")
+    elif e is None:
+      data = yf.download(ticker, start=s) # Only start date is defined
+    elif s is None:
+      data = yf.download(ticker, end=e)  # When only end date is defined
+    else:
+      # When both start date and end date are defined
+      data = yf.download(ticker, start=s, end=e)
 
-        # Extract the Adjusted Close prices and add to the DataFrame
-        if not data.empty:
-            p[ticker] = data['Adj Close']
-
-    p = p.dropna() # Drop rows with NA values
+    # Extract the Adjusted Close prices and add to the DataFrame
+    if not data.empty:
+      p[ticker] = data[('Close', f'{ticker}')]
+            
+  p = p.dropna() # Drop rows with NA values
   
-    x = np.log(p / p.shift(1)).dropna() # Returns
-    M = x.corr() # Correlation Matrix
+  x = np.log(p / p.shift(1)).dropna() # Returns
   
-    plt.figure(figsize=(10, 8)) # Create the heatmap
-    sns.heatmap(M, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
-    plt.title('Stock Returns Correlation Heatmap') # Add labels and title
-    plt.show() # Show
+  if method == "pearson":
+    M = x.corr(method='pearson')  # Pearson correlation
+  
+  else:
+    M = x.corr(method='spearman') # Spearman correlation
+  
+  plt.figure(figsize=(10, 8)) # Create the heatmap
+  sns.heatmap(M, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+  plt.title(f'Stock Returns Heatmap by {method}')
+  plt.show() # Show
     
-heat_map(["XOM", "UNM", "GOOGL", "VIRT"], s = "2023-01-01") # Test
+heat_map(["XOM", "UNM", "GOOGL", "VIRT"], s = "2023-01-01", method="spearman") 
